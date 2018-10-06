e1c9cb5a41192873c7ba9cb4c855f406e666a176
Derek Nguyen <derek@penandpillow.com>
2018-10-07 00:20:59 +0900

    setup

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React, { Component } from 'react';
+import { render } from 'react-dom';
+
+
+const root = document.getElementById('root');
+render(<h1 className="text-red">Wowza!</h1>, root);
